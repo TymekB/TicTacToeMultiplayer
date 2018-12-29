254d5ba7208bb6f89c01433a47ecec98fe626d37
TymekB <baran.tymoteusz@gmail.com>
2018-12-29 14:49:06 +0100

    added createBoard function

---
+++ public/js/main.js
@@ -0,0 +1,10 @@
+function createBoard(element) {
+    for(let i = 0; i < 9; i++) {
+        $(element).append('<div class="square" data-id="'+i+'"></div>');
+    }
+}
+
+$(document).ready(function() {
+    createBoard('#board');
+
+});
